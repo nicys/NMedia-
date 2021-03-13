9f362ffea59822ca844b0838bc8d5f145dc761aa
nicys <nicys2476@yandex.ru>
2021-03-13 19:29:19 +0700

    Added values.

--- app/src/main/java/ru/netology/viewmodel/PostViewModel.kt
+++ app/src/main/java/ru/netology/viewmodel/PostViewModel.kt
@@ -1,4 +1,12 @@
 package ru.netology.viewmodel
 
-class PostViewModel {
+import androidx.lifecycle.ViewModel
+import ru.netology.repository.PostRepository
+import ru.netology.repository.PostRepositoryInMemoryImpl
+
+class PostViewModel : ViewModel() {
+    private val repository: PostRepository = PostRepositoryInMemoryImpl()
+    val data = repository.get()
+    val like = repository.like()
+    val shara = repository.share()
 }
