a002b766fa5a095d3b54ada145595351f2b7b29b
nicys <nicys2476@yandex.ru>
2021-03-16 16:07:39 +0700

    Changed fun's PostViewModel.kt

--- app/src/main/java/ru/netology/viewmodel/PostViewModel.kt
+++ app/src/main/java/ru/netology/viewmodel/PostViewModel.kt
@@ -1,13 +1,12 @@
 package ru.netology.viewmodel
 
-import android.os.Build
 import androidx.lifecycle.ViewModel
 import ru.netology.repository.PostRepository
 import ru.netology.repository.PostRepositoryInMemoryImpl
 
 class PostViewModel : ViewModel() {
     private val repository: PostRepository = PostRepositoryInMemoryImpl()
-    val data = repository.get()
-    fun like() = repository.like()
-    fun share() = repository.share()
+    val data = repository.getAll()
+    fun likeById(id: Long) = repository.likeById(id)
+    fun shareById(id: Long) = repository.shareById(id)
 }
