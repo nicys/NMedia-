e623962336a92a22d7f2660a52ded98a7447e182
nicys <nicys2476@yandex.ru>
2021-03-23 22:19:42 +0700

    Corrected MainActivity.kt

--- app/src/main/java/ru/netology/MainActivity.kt
+++ app/src/main/java/ru/netology/MainActivity.kt
@@ -1,10 +1,15 @@
 package ru.netology
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import ru.netology.adapter.OnInteractionListener
 import ru.netology.adapter.PostsAdapter
 import ru.netology.databinding.ActivityMainBinding
+import ru.netology.dto.Post
+import ru.netology.util.AndroidUtils
 import ru.netology.viewmodel.PostViewModel
 
 class MainActivity : AppCompatActivity() {
@@ -15,13 +20,67 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val viewModel: PostViewModel by viewModels()
-        val adapter = PostsAdapter(
-                onLikeListener = { viewModel.likeById(it.id) },
-                onShareListener = { viewModel.shareById(it.id) }
-        )
+        val adapter = PostsAdapter(object : OnInteractionListener {
+            override fun onEdit(post: Post) {
+                binding.cancel.visibility = View.VISIBLE
+                viewModel.edit(post)
+
+            }
+
+            override fun onLike(post: Post) {
+                viewModel.likeById(post.id)
+            }
+
+            override fun onRemove(post: Post) {
+                viewModel.removeById(post.id)
+            }
+
+            override fun onShare(post: Post) {
+                viewModel.shareById(post.id)
+            }
+        })
         binding.list.adapter = adapter
         viewModel.data.observe(this, { posts ->
             adapter.submitList(posts)
         })
+
+        viewModel.edited.observe(this, { post ->
+            if (post.id == 0L) {
+                return@observe
+            }
+            with(binding.contentPost) {
+                requestFocus()
+                setText(post.content)
+            }
+        })
+
+        binding.save.setOnClickListener {
+            with(binding.contentPost) {
+                if (text.isNullOrBlank()) {
+                    Toast.makeText(
+                            this@MainActivity,
+                            context.getString(R.string.error_empty_content),
+                            Toast.LENGTH_SHORT
+                    ).show()
+                    return@setOnClickListener
+                }
+
+                viewModel.changeContent(text.toString())
+                viewModel.save()
+
+                setText("")
+                clearFocus()
+                AndroidUtils.hideKeyboard(this)
+                binding.cancel.visibility = View.INVISIBLE
+            }
+        }
+
+        binding.cancel.setOnClickListener {
+            viewModel.edited.observe(this, { post ->
+                with(binding.contentPost) {
+                    setText(post.content)
+                }
+            })
+        }
     }
 }
