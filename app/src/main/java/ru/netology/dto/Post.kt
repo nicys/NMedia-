6bdf0de69f13587402ad158caad688853446dcce
nicys <nicys2476@yandex.ru>
2021-04-03 16:30:16 +0700

    apply direction @Parcelize in Post.kt

--- app/src/main/java/ru/netology/dto/Post.kt
+++ app/src/main/java/ru/netology/dto/Post.kt
@@ -1,5 +1,9 @@
 package ru.netology.dto
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Post(
     val id: Long,
     val author: String,
@@ -8,4 +12,4 @@ data class Post(
     val likeByMe: Boolean,
     val shares: String,
     val sharesCnt: Int
-)
+) : Parcelable
