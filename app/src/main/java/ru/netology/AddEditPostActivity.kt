e634230fd76952f811048367ecb0d89ce10a45c6
nicys <nicys2476@yandex.ru>
2021-04-04 20:37:43 +0700

    added codes

--- app/src/main/java/ru/netology/AddEditPostActivity.kt
+++ app/src/main/java/ru/netology/AddEditPostActivity.kt
@@ -1,11 +1,43 @@
 package ru.netology
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import ru.netology.databinding.ActivityAddEditPostBinding
+import ru.netology.util.AndroidUtils.hideKeyboard
 
 class AddEditPostActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_edit_post)
+        val binding = ActivityAddEditPostBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.edit.setSelectAllOnFocus(true)
+        binding.edit.requestFocus()
+
+        intent?.let {
+            if (it.action != Intent.ACTION_SEND) {
+                return@let
+            }
+            val text = it.getStringExtra(Intent.EXTRA_TEXT)
+            with(binding.edit) {
+                setText(text)
+//                requestFocus()
+                hideKeyboard(this)
+            }
+        }
+
+        binding.ok.setOnClickListener {
+            val intent = Intent()
+            if (binding.edit.text.isNullOrBlank()) {
+                setResult(Activity.RESULT_CANCELED, intent)
+            } else {
+                val content = binding.edit.text.toString()
+                intent.putExtra(Intent.EXTRA_TEXT, content)
+                setResult(Activity.RESULT_OK, intent)
+            }
+            finish()
+        }
     }
 }
