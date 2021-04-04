76fe66d7bf834221e9bf97340de0e39582410187
nicys <nicys2476@yandex.ru>
2021-04-04 23:11:22 +0700

    commented

--- app/src/main/java/ru/netology/MainActivity.kt
+++ app/src/main/java/ru/netology/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : AppCompatActivity() {
         val viewModel: PostViewModel by viewModels()
         val adapter = PostsAdapter(object : OnInteractionListener {
             override fun onEdit(post: Post) {
-                binding.cancel.visibility = View.VISIBLE
+//                binding.cancel.visibility = View.VISIBLE
                 viewModel.edit(post)
             }
 
@@ -62,40 +62,40 @@ class MainActivity : AppCompatActivity() {
 //            }
 //        })
 
-        binding.save.setOnClickListener {
-            with(binding.contentPost) {
-                if (text.isNullOrBlank()) {
-                    Toast.makeText(
-                            this@MainActivity,
-                            context.getString(R.string.error_empty_content),
-                            Toast.LENGTH_SHORT
-                    ).show()
-                    return@setOnClickListener
-                }
-
-                viewModel.changeContent(text.toString())
-                viewModel.save()
-
-                setText("")
-                clearFocus()
-                AndroidUtils.hideKeyboard(this)
-                binding.cancel.visibility = View.INVISIBLE
-            }
-        }
-
-        binding.contentPost.setOnFocusChangeListener { _, hasFocus ->
-            binding.cancel.visibility = if (hasFocus) View.VISIBLE else View.INVISIBLE
-        }
-
-        binding.cancel.setOnClickListener {
-            with(binding.contentPost) {
-                setText("")
-                clearFocus()
-                AndroidUtils.hideKeyboard(this)
-
-            }
-        }
-        binding.cancel.visibility = View.INVISIBLE
+//        binding.save.setOnClickListener {
+//            with(binding.contentPost) {
+//                if (text.isNullOrBlank()) {
+//                    Toast.makeText(
+//                            this@MainActivity,
+//                            context.getString(R.string.error_empty_content),
+//                            Toast.LENGTH_SHORT
+//                    ).show()
+//                    return@setOnClickListener
+//                }
+//
+//                viewModel.changeContent(text.toString())
+//                viewModel.save()
+//
+//                setText("")
+//                clearFocus()
+//                AndroidUtils.hideKeyboard(this)
+//                binding.cancel.visibility = View.INVISIBLE
+//            }
+//        }
+//
+//        binding.contentPost.setOnFocusChangeListener { _, hasFocus ->
+//            binding.cancel.visibility = if (hasFocus) View.VISIBLE else View.INVISIBLE
+//        }
+//
+//        binding.cancel.setOnClickListener {
+//            with(binding.contentPost) {
+//                setText("")
+//                clearFocus()
+//                AndroidUtils.hideKeyboard(this)
+//
+//            }
+//        }
+//        binding.cancel.visibility = View.INVISIBLE
 
         val newPostLauncher = registerForActivityResult(NewPostResultContract()) { result ->
             result ?: return@registerForActivityResult
