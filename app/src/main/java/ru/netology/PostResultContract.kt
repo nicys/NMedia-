beb88e4326ba532910e0c1513b80684f74fa6c80
nicys <nicys2476@yandex.ru>
2021-04-05 13:04:11 +0700

    corrected

--- app/src/main/java/ru/netology/PostResultContract.kt
+++ app/src/main/java/ru/netology/PostResultContract.kt
@@ -18,3 +18,17 @@ class NewPostResultContract : ActivityResultContract<Unit, String?>() {
                 null
             }
 }
+
+class EditPostResultContract : ActivityResultContract<String, String?>() {
+    override fun createIntent(context: Context, input: String): Intent =
+        Intent(context, AddEditPostActivity::class.java)
+            .setAction(Intent.ACTION_SEND)
+            .putExtra(Intent.EXTRA_TEXT, input)
+
+    override fun parseResult(resultCode: Int, intent: Intent?): String? =
+        if (resultCode == Activity.RESULT_OK) {
+            intent?.getStringExtra(Intent.EXTRA_TEXT)
+        } else {
+            null
+        }
+}
