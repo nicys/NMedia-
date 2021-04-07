8d4198c8a5c7b90f1e865abaf60135bd93246a1c
nicys <nicys2476@yandex.ru>
2021-04-07 21:12:42 +0700

    added activity

---
+++ app/src/main/java/ru/netology/AddEditPostActivity.kt
@@ -0,0 +1,11 @@
+package ru.netology
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddEditPostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_edit_post)
+    }
+}
