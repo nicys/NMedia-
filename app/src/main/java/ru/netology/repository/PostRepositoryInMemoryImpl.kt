a8f23ebf3b46c1324e64bb8266df492d48c3231d
nicys <nicys2476@yandex.ru>
2021-04-10 21:05:28 +0700

    comented fun counterOverThousand(feed: Int) and fun totalizerSmartFeed(feed: Int)

--- app/src/main/java/ru/netology/repository/PostRepositoryInMemoryImpl.kt
+++ app/src/main/java/ru/netology/repository/PostRepositoryInMemoryImpl.kt
@@ -143,17 +143,17 @@ class PostRepositoryInMemoryImpl : PostRepository {
     }
 }
 
-fun counterOverThousand(feed: Int): Int {
-    return when(feed) {
-        in 1_000..999_999 -> feed/100
-        else -> feed/100_000
-    }
-}
-
-fun totalizerSmartFeed(feed: Int): String {
-    return when(feed) {
-        in 0..999 -> "$feed"
-        in 1_000..999_999 -> "${ (counterOverThousand(feed).toDouble() / 10) }K"
-        else -> "${ (counterOverThousand(feed).toDouble() / 10) }M"
-    }
-}
+//fun counterOverThousand(feed: Int): Int {
+//    return when(feed) {
+//        in 1_000..999_999 -> feed/100
+//        else -> feed/100_000
+//    }
+//}
+//
+//fun totalizerSmartFeed(feed: Int): String {
+//    return when(feed) {
+//        in 0..999 -> "$feed"
+//        in 1_000..999_999 -> "${ (counterOverThousand(feed).toDouble() / 10) }K"
+//        else -> "${ (counterOverThousand(feed).toDouble() / 10) }M"
+//    }
+//}
