503760afbce0e9de7f1bba295afa3bc651889bca
nicys <nicys2476@yandex.ru>
2021-04-10 19:18:15 +0700

    added video.setOnClickListener

--- app/src/main/java/ru/netology/adapter/PostAdapter.kt
+++ app/src/main/java/ru/netology/adapter/PostAdapter.kt
@@ -51,6 +51,9 @@ class PostViewHolder(
             share.setOnClickListener {
                 onInteractionListener.onShare(post)
             }
+            video.setOnClickListener {
+                onInteractionListener.onVideo(post)
+            }
             menu.setOnClickListener {
                 PopupMenu(it.context, it).apply {
                     inflate(R.menu.option_post)
