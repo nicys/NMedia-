10aa0cbca688f2c6a10dafe5d477a08bf5450abf
nicys <nicys2476@yandex.ru>
2021-04-18 17:43:05 +0700

    corrected code

--- app/src/main/java/ru/netology/FeedFragment.kt
+++ app/src/main/java/ru/netology/FeedFragment.kt
@@ -9,11 +9,14 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import ru.netology.AddEditPostFragment.Companion.textData
+import ru.netology.ShowPostFragment.Companion.postData
 import ru.netology.adapter.OnInteractionListener
 import ru.netology.adapter.PostsAdapter
 import ru.netology.databinding.FragmentFeedBinding
 import ru.netology.dto.Post
 import ru.netology.viewmodel.PostViewModel
+
 
 class FeedFragment : Fragment() {
 
@@ -29,6 +32,9 @@ class FeedFragment : Fragment() {
         val adapter = PostsAdapter(object : OnInteractionListener {
             override fun onEdit(post: Post) {
                 viewModel.edit(post)
+                findNavController().navigate(R.id.action_feedFragment_to_addEditPostFragment,
+                    Bundle().apply { textData = post.content }
+                )
             }
 
             override fun onLike(post: Post) {
@@ -42,10 +48,11 @@ class FeedFragment : Fragment() {
             override fun onShare(post: Post) {
                 val intent = Intent().apply {
                     action = Intent.ACTION_SEND
-                    putExtra(Intent.EXTRA_TEXT, post.published)
+                    putExtra(Intent.EXTRA_TEXT, post.content)
                     type = "text/plain"
                 }
-                val shareIntent = Intent.createChooser(intent, getString(R.string.chooser_share_post))
+                val shareIntent =
+                    Intent.createChooser(intent, getString(R.string.chooser_share_post))
                 startActivity(shareIntent)
                 viewModel.shareById(post.id)
             }
@@ -57,31 +64,37 @@ class FeedFragment : Fragment() {
                         Intent(Intent.ACTION_VIEW, Uri.parse("url"))
                         setData(Uri.parse(post.video))
                     }
-                    val videoIntent = Intent.createChooser(intent, getString(R.string.chooser_video_post))
+                    val videoIntent =
+                        Intent.createChooser(intent, getString(R.string.chooser_video_post))
                     startActivity(videoIntent)
                 }
             }
 
-            override fun onClickPost(post: Post) {
-                findNavController().navigate(R.id.action_feedFragment_to_showPostFragment)
+            override fun onShowPost(post: Post) {
+                findNavController().navigate(R.id.action_feedFragment_to_showPostFragment,
+                    Bundle().apply { postData = post }
+                )
             }
         })
 
         binding.list.adapter = adapter
-        viewModel.data.observe(viewLifecycleOwner, { posts ->
+        viewModel.data.observe(viewLifecycleOwner) { posts ->
             adapter.submitList(posts)
-        })
+        }
 
         binding.fab.setOnClickListener {
             findNavController().navigate(R.id.action_feedFragment_to_addEditPostFragment)
         }
 
-        viewModel.edited.observe(viewLifecycleOwner) { post ->
-            if (post.id == 0L) {
-                return@observe
-            }
-            findNavController().navigate(R.id.action_feedFragment_to_addEditPostFragment)
-        }
+
+
+
+//        viewModel.edited.observe(viewLifecycleOwner) { post ->
+//            if (post.id == 0L) {
+//                return@observe
+//            }
+//            findNavController().navigate(R.id.action_feedFragment_to_addEditPostFragment)
+//        }
 
         return binding.root
     }
