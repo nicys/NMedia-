5a724e2ab158b8a802cca7de21255768632f8c73
nicys <nicys2476@yandex.ru>
2021-04-18 17:54:03 +0700

    added arguments?.postData?.let

--- app/src/main/java/ru/netology/ShowPostFragment.kt
+++ app/src/main/java/ru/netology/ShowPostFragment.kt
@@ -6,21 +6,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.PopupMenu
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
-import ru.netology.AddEditPostFragment.Companion.textArg
-import ru.netology.adapter.OnInteractionListener
-import ru.netology.adapter.PostsAdapter
+import ru.netology.AddEditPostFragment.Companion.textData
 import ru.netology.databinding.FragmentShowPostBinding
 import ru.netology.dto.Post
-import ru.netology.util.StringArg
+import ru.netology.repository.totalizerSmartFeed
+import ru.netology.util.PostArg
 import ru.netology.viewmodel.PostViewModel
 
 class ShowPostFragment : Fragment() {
 
     companion object {
-        var Bundle.postShow: String? by StringArg
+        var Bundle.postData: Post? by PostArg
     }
 
     private val viewModel: PostViewModel by viewModels(
@@ -34,65 +34,18 @@ class ShowPostFragment : Fragment() {
     ): View {
         val binding = FragmentShowPostBinding.inflate(inflater, container, false)
 
-//        arguments?.postShow?.let {
-//            binding.postList.setText(it)
-//        }
+        arguments?.postData?.let {
+            val showPost = it
+            with(binding) {
+                author.text = showPost.author
+                published.text = showPost.published
+                content.text = showPost.content
+                share.text = totalizerSmartFeed(showPost.sharesCnt)
+                like.isChecked = showPost.likeByMe
+                like.text = if (showPost.likeByMe) "1" else "0"
 
-        val adapter = PostsAdapter(object : OnInteractionListener {
-            override fun onEdit(post: Post) {
-                viewModel.edit(post)
             }
-
-            override fun onLike(post: Post) {
-                viewModel.likeById(post.id)
-            }
-
-            override fun onRemove(post: Post) {
-                viewModel.removeById(post.id)
-            }
-
-            override fun onShare(post: Post) {
-                val intent = Intent().apply {
-                    action = Intent.ACTION_SEND
-                    putExtra(Intent.EXTRA_TEXT, post.published)
-                    type = "text/plain"
-                }
-                val shareIntent =
-                    Intent.createChooser(intent, getString(R.string.chooser_share_post))
-                startActivity(shareIntent)
-                viewModel.shareById(post.id)
-            }
-
-            override fun onVideo(post: Post) {
-                post.video?.let {
-                    val intent = Intent().apply {
-                        action = Intent.ACTION_VIEW
-                        Intent(Intent.ACTION_VIEW, Uri.parse("url"))
-                        setData(Uri.parse(post.video))
-                    }
-                    val videoIntent =
-                        Intent.createChooser(intent, getString(R.string.chooser_video_post))
-                    startActivity(videoIntent)
-                }
-            }
-
-            override fun onClickPost(post: Post) {
-                findNavController().navigate(R.id.action_feedFragment_to_showPostFragment)
-            }
-        })
-
-//        binding.postListScroll.adapter = adapter
-//        viewModel.data.observe(viewLifecycleOwner, { post ->
-//            adapter.submitList(post)
-//        })
-
-//        viewModel.edited.observe(viewLifecycleOwner) { post ->
-//            if (post.id == 0L) {
-//                return@observe
-//            }
-//        }
-
-
+        }
         return binding.root
     }
 }
