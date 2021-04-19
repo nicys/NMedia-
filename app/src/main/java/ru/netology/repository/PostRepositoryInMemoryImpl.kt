1236b8068e7b304bda996011d28d994368753295
nicys <nicys2476@yandex.ru>
2021-04-19 14:27:35 +0700

    added val like in Post()

--- app/src/main/java/ru/netology/repository/PostRepositoryInMemoryImpl.kt
+++ app/src/main/java/ru/netology/repository/PostRepositoryInMemoryImpl.kt
@@ -13,6 +13,7 @@ class PostRepositoryInMemoryImpl : PostRepository {
             content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
             published = "21 мая в 18:36",
             likeByMe = false,
+            like = 0,
             shares = "0",
             sharesCnt = 0,
             video = "https://www.youtube.com/watch?v=25Kt-9fwnVw"
@@ -23,6 +24,7 @@ class PostRepositoryInMemoryImpl : PostRepository {
             content = "Знаний хватит на всех: на следующей неделе разбираемся с разработкой мобильных приложений, учимся рассказывать истории и составлять PR-стратегию прямо на бесплатных занятиях \uD83D\uDC47",
             published = "18 сентября в 10:12",
             likeByMe = false,
+            like = 0,
             shares = "0",
             sharesCnt = 0,
             video = "https://www.youtube.com/watch?v=scA2Fn7DeUw"
@@ -33,6 +35,7 @@ class PostRepositoryInMemoryImpl : PostRepository {
             content = "Языков программирования много, и выбрать какой-то один бывает нелегко. Собрали подборку статей, которая поможет вам начать, если вы остановили свой выбор на JavaScript.",
             published = "19 сентября в 10:24",
             likeByMe = false,
+            like = 0,
             shares = "0",
             sharesCnt = 0,
             video = "https://www.youtube.com/watch?v=_x1JIciP93s"
@@ -43,6 +46,7 @@ class PostRepositoryInMemoryImpl : PostRepository {
             content = "Большая афиша мероприятий осени: конференции, выставки и хакатоны для жителей Москвы, Ульяновска и Новосибирска \uD83D\uDE09",
             published = "19 сентября в 14:12",
             likeByMe = false,
+            like = 0,
             shares = "0",
             sharesCnt = 0,
             video = "https://www.youtube.com/watch?v=6h2lNTEr07w"
@@ -53,6 +57,7 @@ class PostRepositoryInMemoryImpl : PostRepository {
             content = "Диджитал давно стал частью нашей жизни: мы общаемся в социальных сетях и мессенджерах, заказываем еду, такси и оплачиваем счета через приложения.",
             published = "20 сентября в 10:14",
             likeByMe = false,
+            like = 0,
             shares = "0",
             sharesCnt = 0,
             video = "https://www.youtube.com/watch?v=UdLX91MyiMI"
@@ -63,6 +68,7 @@ class PostRepositoryInMemoryImpl : PostRepository {
             content = "\uD83D\uDE80 24 сентября стартует новый поток бесплатного курса «Диджитал-старт: первый шаг к востребованной профессии» — за две недели вы попробуете себя в разных профессиях и определите, что подходит именно вам → http://netolo.gy/fQ",
             published = "21 сентября в 10:12",
             likeByMe = false,
+            like = 0,
             shares = "0",
             sharesCnt = 0,
             video = "https://www.youtube.com/watch?v=7TmXNz0E1CU"
@@ -73,6 +79,7 @@ class PostRepositoryInMemoryImpl : PostRepository {
             content = "Таймбоксинг — отличный способ навести порядок в своём календаре и разобраться с делами, которые долго откладывали на потом. Его главный принцип — на каждое дело заранее выделяется определённый отрезок времени. В это время вы работаете только над одной задачей, не переключаясь на другие. Собрали советы, которые помогут внедрить таймбоксинг \uD83D\uDC47\uD83C\uDFFB",
             published = "22 сентября в 10:12",
             likeByMe = false,
+            like = 0,
             shares = "0",
             sharesCnt = 0,
             video = "https://www.youtube.com/watch?v=77guk633rzc"
@@ -83,6 +90,7 @@ class PostRepositoryInMemoryImpl : PostRepository {
             content = "Делиться впечатлениями о любимых фильмах легко, а что если рассказать так, чтобы все заскучали \uD83D\uDE34\n",
             published = "22 сентября в 10:14",
             likeByMe = false,
+            like = 0,
             shares = "0",
             sharesCnt = 0,
             video = "https://www.youtube.com/watch?v=s-EbpBXeJaQ"
@@ -93,6 +101,7 @@ class PostRepositoryInMemoryImpl : PostRepository {
             content = "Освоение новой профессии — это не только открывающиеся возможности и перспективы, но и настоящий вызов самому себе. Приходится выходить из зоны комфорта и перестраивать привычный образ жизни: менять распорядок дня, искать время для занятий, быть готовым к возможным неудачам в начале пути. В блоге рассказали, как избежать стресса на курсах профпереподготовки → http://netolo.gy/fPD",
             published = "23 сентября в 10:12",
             likeByMe = false,
+            like = 0,
             shares = "0",
             sharesCnt = 0,
             video = "https://www.youtube.com/watch?v=2W4N9Jf4fj0"
