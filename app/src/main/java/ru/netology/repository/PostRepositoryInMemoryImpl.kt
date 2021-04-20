f05ffe542b9e186dc6146a2a129528acc6c8fcfc
nicys <nicys2476@yandex.ru>
2021-04-20 18:39:51 +0700

    corrected

--- app/src/main/java/ru/netology/repository/PostRepositoryInMemoryImpl.kt
+++ app/src/main/java/ru/netology/repository/PostRepositoryInMemoryImpl.kt
@@ -150,19 +150,20 @@ class PostRepositoryInMemoryImpl : PostRepository {
         }
         data.value = posts
     }
+
+    private fun counterOverThousand(feed: Int): Int {
+        return when(feed) {
+            in 1_000..999_999 -> feed/100
+            else -> feed/100_000
+        }
+    }
+
+    private fun totalizerSmartFeed(feed: Int): String {
+        return when(feed) {
+            in 0..999 -> "$feed"
+            in 1_000..999_999 -> "${ (counterOverThousand(feed).toDouble() / 10) }K"
+            else -> "${ (counterOverThousand(feed).toDouble() / 10) }M"
+        }
+    }
 }
 
-//fun counterOverThousand(feed: Int): Int {
-//    return when(feed) {
-//        in 1_000..999_999 -> feed/100
-//        else -> feed/100_000
-//    }
-//}
-//
-//fun totalizerSmartFeed(feed: Int): String {
-//    return when(feed) {
-//        in 0..999 -> "$feed"
-//        in 1_000..999_999 -> "${ (counterOverThousand(feed).toDouble() / 10) }K"
-//        else -> "${ (counterOverThousand(feed).toDouble() / 10) }M"
-//    }
-//}
