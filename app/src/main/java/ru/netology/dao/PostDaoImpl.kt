b01084d2e04e9db32c77c9946285e1da68e3f2f3
nicys <nicys2476@yandex.ru>
2021-04-20 18:36:23 +0700

    added columns

--- app/src/main/java/ru/netology/dao/PostDaoImpl.kt
+++ app/src/main/java/ru/netology/dao/PostDaoImpl.kt
@@ -65,12 +65,12 @@ class PostDaoImpl(private val db: SQLiteDatabase) : PostDao {
         return posts
     }
 
-
     override fun save(post: Post): Post {
         val values = ContentValues().apply {
             if (post.id != 0L) {
                 put(PostColumns.COLUMN_ID, post.id)
             }
+            // TODO: remove hardcoded values
             put(PostColumns.COLUMN_AUTHOR, "Me")
             put(PostColumns.COLUMN_CONTENT, post.content)
             put(PostColumns.COLUMN_PUBLISHED, "now")
