10de939fd8b979ed7b9215f46be0811009990db6
nicys <nicys2476@yandex.ru>
2021-04-21 21:23:19 +0700

    added viewModel return@observe

--- app/src/main/java/ru/netology/ShowPostFragment.kt
+++ app/src/main/java/ru/netology/ShowPostFragment.kt
@@ -11,11 +11,8 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import ru.netology.AddEditPostFragment.Companion.textData
-import ru.netology.adapter.OnInteractionListener
-import ru.netology.adapter.PostsAdapter
 import ru.netology.databinding.FragmentShowPostBinding
 import ru.netology.dto.Post
-import ru.netology.repository.totalizerSmartFeed
 import ru.netology.util.PostArg
 import ru.netology.viewmodel.PostViewModel
 
@@ -36,21 +33,75 @@ class ShowPostFragment : Fragment() {
     ): View {
         val binding = FragmentShowPostBinding.inflate(inflater, container, false)
 
-        val adapter = PostsAdapter(object : OnInteractionListener {
+        arguments?.postData?.let {
+            val showPost = it
+            viewModel.getPostById(it.id).observe(viewLifecycleOwner, { post ->
+                post ?: return@observe
+                binding.apply {
+                    author.text = post.author
+                    published.text = post.published
+                    content.text = post.content
+                    share.text = viewModel.totalizerSmartFeed(post.sharesCnt)
+                    like.isChecked = post.likeByMe
+                    like.text = if (post.likeByMe) "1" else "0"
+                    menu.setOnClickListener {
+                        PopupMenu(it.context, it).apply {
+                            inflate(R.menu.option_post)
+                            setOnMenuItemClickListener { item ->
+                                when (item.itemId) {
+                                    R.id.remove -> {
+                                        viewModel.removeById(post.id)
+                                        findNavController().navigate(R.id.action_showPostFragment_to_feedFragment)
+                                        true
+                                    }
+                                    R.id.edit -> {
+                                        findNavController().navigate(R.id.action_showPostFragment_to_addEditPostFragment,
+                                            Bundle().apply
+                                            { textData = post.content })
+                                        true
+                                    }
+                                    else -> false
+                                }
+                            }
+                        }.show()
+                    }
+                    like.setOnClickListener {
+                        viewModel.likeById(showPost.id)
+                    }
+                    share.setOnClickListener {
+                        val intent = Intent().apply {
+                            action = Intent.ACTION_SEND
+                            putExtra(Intent.EXTRA_TEXT, showPost.published)
+                            type = "text/plain"
+                        }
+                        val shareIntent =
+                            Intent.createChooser(intent, getString(R.string.chooser_share_post))
+                        startActivity(shareIntent)
+                        viewModel.shareById(showPost.id)
+                    }
+                    video.setOnClickListener {
+                        post.video?.let {
+                            val intent = Intent().apply {
+                                action = Intent.ACTION_VIEW
+                                Intent(Intent.ACTION_VIEW, Uri.parse("url"))
+                                data = Uri.parse(showPost.video)
+                            }
+                            val videoIntent =
+                                Intent.createChooser(intent, getString(R.string.chooser_video_post))
+                            startActivity(videoIntent)
+                        }
+                    }
 
-            
-        })
+                }
+            })
+        }
 
-        binding.postList.adapter = adapter
-        viewModel.data.observe(viewLifecycleOwner, { posts ->
-            adapter.submitList(posts)
-        })
+
 
 
         return binding.root
     }
 }
-
 
 
 //arguments?.postData?.let {
