5e39a80ff2d3eb6e4e0fb12ef77b243d3bd4bea5
nicys <nicys2476@yandex.ru>
2021-04-22 10:29:26 +0700

    initial

--- app/src/main/java/ru/netology/dao/PostDao.kt
+++ app/src/main/java/ru/netology/dao/PostDao.kt
@@ -1,11 +1,11 @@
 package ru.netology.nmedia.dao
 
-import ru.netology.nmedia.dto.Post
-import java.io.Closeable
+import ru.netology.dto.Post
 
 interface PostDao {
     fun getAll(): List<Post>
-    fun save(post: Post): Post
     fun likeById(id: Long)
+    fun shareById(id: Long)
     fun removeById(id: Long)
+    fun save(post: Post): Post
 }
