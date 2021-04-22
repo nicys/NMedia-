a3e765920c96de09772160e4956c4ba43486b308
nicys <nicys2476@yandex.ru>
2021-04-22 09:52:44 +0700

    corrected

--- app/src/main/java/ru/netology/repository/PostRepositoryFileImpl.kt
+++ app/src/main/java/ru/netology/repository/PostRepositoryFileImpl.kt
@@ -80,4 +80,19 @@ class PostRepositoryFileImpl(
             it.write(gson.toJson(posts))
         }
     }
+
+    private fun counterOverThousand(feed: Int): Int {
+        return when (feed) {
+            in 1_000..999_999 -> feed / 100
+            else -> feed / 100_000
+        }
+    }
+
+    private fun totalizerSmartFeed(feed: Int): String {
+        return when (feed) {
+            in 0..999 -> "$feed"
+            in 1_000..999_999 -> "${(counterOverThousand(feed).toDouble() / 10)}K"
+            else -> "${(counterOverThousand(feed).toDouble() / 10)}M"
+        }
+    }
 }
