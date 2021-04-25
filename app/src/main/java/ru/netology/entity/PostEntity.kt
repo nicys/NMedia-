39bbf6e40eaae6ada3703860ae771f85774ffaad
nicys <nicys2476@yandex.ru>
2021-04-25 10:54:01 +0700

    added PostEntity.kt

--- app/src/main/java/ru/netology/entity/PostEntity.kt
+++ app/src/main/java/ru/netology/entity/PostEntity.kt
@@ -1,4 +1,18 @@
 package ru.netology.entity
 
-class PostEntity {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+data class PostEntity(
+    @PrimaryKey(autoGenerate = true)
+    val id: Long,
+    val author: String,
+    val published: String,
+    val content: String,
+    val likeByMe: Boolean,
+    val like: Int,
+    val shares: String,
+    val sharesCnt: Int,
+    val video: String?
+)
