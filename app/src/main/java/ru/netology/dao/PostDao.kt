7faecbf05ca4b968744d4e4451fdb1e22fd1fd04
nicys <nicys2476@yandex.ru>
2021-04-25 11:24:27 +0700

    added

--- app/src/main/java/ru/netology/dao/PostDao.kt
+++ app/src/main/java/ru/netology/dao/PostDao.kt
@@ -1,14 +1,37 @@
-package ru.netology.nmedia.dao
+package ru.netology.dao
 
 import androidx.lifecycle.LiveData
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import ru.netology.dto.Post
+import ru.netology.entity.PostEntity
 
 interface PostDao {
     @Query("SELECT * FROM PostEntity ORDER BY id")
-    fun getAll(): LiveData<List<Post>>
+    fun getAll(): LiveData<List<PostEntity>>
+
+    @Query(
+        """
+        UPDATE PostEntity SET
+        like = like + CASE WHEN likeByMe THEN -1 ELSE 1 END,
+        likeByMe = CASE WHEN likeByMe THEN 0 ELSE 1 END
+        WHERE id = :id
+        """
+    )
     fun likeById(id: Long)
+
+    @Query(
+        """
+        UPDATE PostEntity SET
+        sharesCnt = sharesCnt + 1
+        WHERE id = :id
+        """
+    )
     fun shareById(id: Long)
+
+    @Query("DELETE FROM PostEntity ORDER BY id")
     fun removeById(id: Long)
-    fun save(post: Post): Post
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun save(post: PostEntity): Long
 }
