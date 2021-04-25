10fca433e18cc695c2c88e18a0012e10e2d65380
nicys <nicys2476@yandex.ru>
2021-04-25 11:00:49 +0700

    added LiveData to fun getAll() and  @Query

--- app/src/main/java/ru/netology/dao/PostDao.kt
+++ app/src/main/java/ru/netology/dao/PostDao.kt
@@ -1,9 +1,12 @@
 package ru.netology.nmedia.dao
 
+import androidx.lifecycle.LiveData
+import androidx.room.Query
 import ru.netology.dto.Post
 
 interface PostDao {
-    fun getAll(): List<Post>
+    @Query("SELECT * FROM PostEntity ORDER BY id")
+    fun getAll(): LiveData<List<Post>>
     fun likeById(id: Long)
     fun shareById(id: Long)
     fun removeById(id: Long)
