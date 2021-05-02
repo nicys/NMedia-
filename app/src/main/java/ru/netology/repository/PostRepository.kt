5ecff0ab5e64a8e998c1b59798ce6d8154934c49
nicys <nicys2476@yandex.ru>
2021-05-02 11:35:00 +0700

    deleted

--- app/src/main/java/ru/netology/repository/PostRepository.kt
+++ app/src/main/java/ru/netology/repository/PostRepository.kt
@@ -1,9 +1,10 @@
 package ru.netology.repository
 
+import androidx.lifecycle.LiveData
 import ru.netology.dto.Post
 
 interface PostRepository {
-    fun getAll(): List<Post>
+    fun getAll(): LiveData<List<Post>>
     fun likeById(id: Long)
     fun shareById(id: Long)
     fun removeById(id: Long)
