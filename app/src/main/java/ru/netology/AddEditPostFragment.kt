d535a945ba0876e795160abdd7b61522ba8c330e
nicys <nicys2476@yandex.ru>
2021-05-04 18:48:08 +0700

    deleted findNavController().navigateUp() from ok.setOnClickListener in AddEditPostFragment.kt

--- app/src/main/java/ru/netology/AddEditPostFragment.kt
+++ app/src/main/java/ru/netology/AddEditPostFragment.kt
@@ -8,6 +8,7 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import ru.netology.databinding.FragmentAddEditPostBinding
+import ru.netology.util.AndroidUtils
 import ru.netology.util.AndroidUtils.hideKeyboard
 import ru.netology.util.StringArg
 import ru.netology.viewmodel.PostViewModel
@@ -35,7 +36,6 @@ class AddEditPostFragment : Fragment() {
             viewModel.changeContent(binding.edit.text.toString())
             viewModel.save()
             hideKeyboard(requireView())
-            findNavController().navigateUp()
         }
 
         viewModel.postCreated.observe(viewLifecycleOwner) {
