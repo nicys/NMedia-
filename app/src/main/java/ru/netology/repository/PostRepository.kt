eb971c79454dd1b13bcca2fcd3ca03692ba073a8
nicys <nicys2476@yandex.ru>
2021-05-17 16:59:21 +0700

    added funs

--- app/src/main/java/ru/netology/repository/PostRepository.kt
+++ app/src/main/java/ru/netology/repository/PostRepository.kt
@@ -4,16 +4,32 @@ import androidx.lifecycle.LiveData
 import ru.netology.dto.Post
 
 interface PostRepository {
-    fun getAll(): List<Post>
-    fun likeById(id: Long)
-    fun shareById(id: Long)
-    fun removeById(id: Long)
-    fun save(post: Post)
+//    fun getAll(): List<Post>
+//    fun likeById(id: Long)
+//    fun shareById(id: Long)
+//    fun removeById(id: Long)
+//    fun save(post: Post)
 
     fun getAllAsync(callback: GetAllCallback)
 
     interface GetAllCallback {
         fun onSuccess(posts: List<Post>) {}
+        fun onError(e: Exception) {}
+    }
+
+    fun likeById(id: Long, callback: GetIdCallback)
+    fun shareById(id: Long, callback: GetIdCallback)
+    fun removeById(id: Long, callback: GetIdCallback)
+
+    interface GetIdCallback {
+        fun onSuccess(id: Long) {}
+        fun onError(e: Exception) {}
+    }
+
+    fun save(post: Post, callback: GetPostCallback)
+
+    interface GetPostCallback {
+        fun onSuccess(post: Post) {}
         fun onError(e: Exception) {}
     }
 }
