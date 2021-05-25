ecd4a9c0a4e24c42df958486faa49e903876d377
nicys <nicys2476@yandex.ru>
2021-05-25 15:53:17 +0700

    corrected

--- app/src/main/java/ru/netology/repository/PostRepository.kt
+++ app/src/main/java/ru/netology/repository/PostRepository.kt
@@ -1,24 +1,10 @@
 package ru.netology.repository
 
-import retrofit2.Callback
-import retrofit2.http.POST
 import ru.netology.dto.Post
 
 interface PostRepository {
 
-//    fun getAll(): List<Post>
-//    fun likeById(id: Long)
-//    fun shareById(id: Long)
-//    fun removeById(id: Long)
-//    fun save(post: Post)
-
     fun getAllAsync(callback: Callback<List<Post>>)
-
-//    interface GetAllCallback {
-//        fun onSuccess(posts: List<Post>) {}
-//        fun onError(e: Exception) {}
-//    }
-
     fun likeByIdAsyn(callback: Callback<Post>, id: Long)
     fun dislikeByIdAsyn(callback: Callback<Post>, id: Long)
     fun shareByIdAsyn(callback: Callback<Post>, id: Long)
