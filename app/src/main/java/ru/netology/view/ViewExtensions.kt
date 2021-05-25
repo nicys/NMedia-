296023e5d0d965752889914e8d0b438028069371
nicys <nicys2476@yandex.ru>
2021-05-25 18:28:17 +0700

    added ic_avatar_error

--- app/src/main/java/ru/netology/view/ViewExtensions.kt
+++ app/src/main/java/ru/netology/view/ViewExtensions.kt
@@ -4,11 +4,14 @@ import android.widget.ImageView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.BitmapTransformation
 import com.bumptech.glide.load.resource.bitmap.CircleCrop
+import ru.netology.R
 
 fun ImageView.load(url: String, vararg transforms: BitmapTransformation = emptyArray()) =
     Glide.with(this)
         .load(url)
-        .timeout(1_000)
+        .timeout(10_000)
+
+        .error(R.drawable.ic_netology_48)
         .transform(*transforms)
         .into(this)
 
