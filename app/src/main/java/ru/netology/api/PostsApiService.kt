176912fde0c48ee4367f7710355bf782c5439a50
nicys <nicys2476@yandex.ru>
2021-06-04 21:02:45 +0700

    changed Call on Responce

--- app/src/main/java/ru/netology/api/PostsApiService.kt
+++ app/src/main/java/ru/netology/api/PostsApiService.kt
@@ -2,7 +2,7 @@ package ru.netology.api
 
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
-import retrofit2.Call
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.*
@@ -29,25 +29,25 @@ private val retrofit = Retrofit.Builder()
 
 interface PostsApiService {
     @GET("posts")
-    suspend fun getAll(): Call<List<Post>>
+    suspend fun getAll(): Response<List<Post>>
 
     @GET("posts/{id}")
-    suspend fun getById(@Path("id") id: Long): Call<Post>
+    suspend fun getById(@Path("id") id: Long): Response<Post>
 
     @POST("posts")
-    suspend fun save(@Body post: Post): Call<Post>
+    suspend fun save(@Body post: Post): Response<Post>
 
     @DELETE("posts/{id}")
-    suspend fun removeById(@Path("id") id: Long): Call<Unit>
+    suspend fun removeById(@Path("id") id: Long): Response<Unit>
 
     @POST("posts/{id}/likes")
-    suspend fun likeById(@Path("id") id: Long): Call<Post>
+    suspend fun likeById(@Path("id") id: Long): Response<Post>
 
     @DELETE("posts/{id}/likes")
-    suspend fun dislikeById(@Path("id") id: Long): Call<Post>
+    suspend fun dislikeById(@Path("id") id: Long): Response<Post>
 
     @POST("posts/{id}/shares")
-    suspend fun shareById(@Path("id") id: Long): Call<Post>
+    suspend fun shareById(@Path("id") id: Long): Response<Post>
 }
 
 object PostsApi {
