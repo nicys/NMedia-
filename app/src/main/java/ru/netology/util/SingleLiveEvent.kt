5241c4c8c75313b8c346140b1a33f3317eca3af8
nicys <nicys2476@yandex.ru>
2021-06-04 23:18:03 +0700

    added annotations @MainThread

--- app/src/main/java/ru/netology/util/SingleLiveEvent.kt
+++ app/src/main/java/ru/netology/util/SingleLiveEvent.kt
@@ -1,19 +1,20 @@
 package ru.netology.util
 
+import androidx.annotation.MainThread
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import java.util.concurrent.atomic.AtomicBoolean
 
 class SingleLiveEvent<T> : MutableLiveData<T>() {
-
     private var pending = AtomicBoolean(false)
-    
+
+    @MainThread
     override fun observe(owner: LifecycleOwner, observer: Observer<in T?>) {
         require (!hasActiveObservers()) {
             error("Multiple observers registered but only one will be notified of changes.")
         }
-        
+
         super.observe(owner) {
             if (pending.compareAndSet(true, false)) {
                 observer.onChanged(it)
@@ -21,6 +22,7 @@ class SingleLiveEvent<T> : MutableLiveData<T>() {
         }
     }
 
+    @MainThread
     override fun setValue(t: T?) {
         pending.set(true)
         super.setValue(t)
