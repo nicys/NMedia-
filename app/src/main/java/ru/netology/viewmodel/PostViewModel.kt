49c6bafd5d889efe3883675b104d5510be6a17e8
nicys <nicys2476@yandex.ru>
2021-06-08 15:14:37 +0700

    corrected

--- app/src/main/java/ru/netology/viewmodel/PostViewModel.kt
+++ app/src/main/java/ru/netology/viewmodel/PostViewModel.kt
@@ -182,22 +182,21 @@ class PostViewModel(application: Application) : AndroidViewModel(application) {
         }
     }
 
-private fun counterOverThousand(feed: Int): Int {
-    return when (feed) {
-        in 1_000..999_999 -> feed / 100
-        else -> feed / 100_000
+    private fun counterOverThousand(feed: Int): Int {
+        return when (feed) {
+            in 1_000..999_999 -> feed / 100
+            else -> feed / 100_000
+        }
+    }
+
+    fun totalizerSmartFeed(feed: Int): String {
+        return when (feed) {
+            in 0..999 -> "$feed"
+            in 1_000..999_999 -> "${(counterOverThousand(feed).toDouble() / 10)}K"
+            else -> "${(counterOverThousand(feed).toDouble() / 10)}M"
+        }
     }
 }
-
-fun totalizerSmartFeed(feed: Int): String {
-    return when (feed) {
-        in 0..999 -> "$feed"
-        in 1_000..999_999 -> "${(counterOverThousand(feed).toDouble() / 10)}K"
-        else -> "${(counterOverThousand(feed).toDouble() / 10)}M"
-    }
-}
-}
-
 
 
 //
