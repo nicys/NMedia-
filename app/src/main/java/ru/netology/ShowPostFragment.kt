bab2e21049ca778b5814b4806c3378cac7e24359
nicys <nicys2476@yandex.ru>
2021-06-10 14:12:45 +0700

    commented

--- app/src/main/java/ru/netology/ShowPostFragment.kt
+++ app/src/main/java/ru/netology/ShowPostFragment.kt
@@ -8,25 +8,25 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import ru.netology.databinding.FragmentShowPostBinding
 import ru.netology.dto.Post
-import ru.netology.util.PostArg
+//import ru.netology.util.PostArg
 import ru.netology.viewmodel.PostViewModel
 
 class ShowPostFragment : Fragment() {
 
-    companion object {
-        var Bundle.postData: Post? by PostArg
-    }
-
-    private val viewModel: PostViewModel by viewModels(
-        ownerProducer = ::requireParentFragment
-    )
-
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View {
-        val binding = FragmentShowPostBinding.inflate(inflater, container, false)
+//    companion object {
+//        var Bundle.postData: Post? by PostArg
+//    }
+//
+//    private val viewModel: PostViewModel by viewModels(
+//        ownerProducer = ::requireParentFragment
+//    )
+//
+//    override fun onCreateView(
+//        inflater: LayoutInflater,
+//        container: ViewGroup?,
+//        savedInstanceState: Bundle?
+//    ): View {
+//        val binding = FragmentShowPostBinding.inflate(inflater, container, false)
 
 //        with(viewModel) {
 //            arguments?.postData?.let {
@@ -100,8 +100,8 @@ class ShowPostFragment : Fragment() {
 //                })
 //            }
 //        }
-        return binding.root
-    }
+//        return binding.root
+//    }
 }
 
 
