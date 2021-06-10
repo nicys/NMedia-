5bce80bf53d2264f17e34635f274630ec4d24168
nicys <nicys2476@yandex.ru>
2021-06-10 13:46:54 +0700

    added suspend fun saveWithAttachment & suspend fun upload

--- app/src/main/java/ru/netology/repository/PostRepository.kt
+++ app/src/main/java/ru/netology/repository/PostRepository.kt
@@ -2,6 +2,8 @@ package ru.netology.repository
 
 import kotlinx.coroutines.flow.Flow
 import ru.netology.dto.Post
+import ru.netology.nmedia.dto.Media
+import ru.netology.nmedia.dto.MediaUpload
 
 interface PostRepository {
 
@@ -9,8 +11,10 @@ interface PostRepository {
     suspend fun getAll()
     fun getNewerCount(id: Long): Flow<Int>
     suspend fun save(post: Post)
+    suspend fun saveWithAttachment(post: Post, upload: MediaUpload)
     suspend fun removeById(id: Long)
     suspend fun likeById(id: Long)
 //    suspend fun unlikeById(id: Long)
     suspend fun shareById(id: Long)
+    suspend fun upload(upload: MediaUpload): Media
 }
