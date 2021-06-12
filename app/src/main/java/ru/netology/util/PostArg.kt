7e1ec03838a99b8c875b991ec8212d33687884eb
nicys <nicys2476@yandex.ru>
2021-06-12 14:00:05 +0700

    unCommented

--- app/src/main/java/ru/netology/util/PostArg.kt
+++ app/src/main/java/ru/netology/util/PostArg.kt
@@ -5,13 +5,12 @@ import ru.netology.dto.Post
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
-//object PostArg: ReadWriteProperty<Bundle, Post?> {
-//    override fun setValue(thisRef: Bundle, property: KProperty<*>, value: Post?) {
-//        thisRef.putParcelable(property.name, value)
-//    }
-//
-//    override fun getValue(thisRef: Bundle, property: KProperty<*>): Post? {
-//        return thisRef.getParcelable(property.name)
-//    }
-//
-//}
+object PostArg: ReadWriteProperty<Bundle, Post?> {
+    override fun setValue(thisRef: Bundle, property: KProperty<*>, value: Post?) {
+        thisRef.putParcelable(property.name, value)
+    }
+
+    override fun getValue(thisRef: Bundle, property: KProperty<*>): Post? {
+        return thisRef.getParcelable(property.name)
+    }
+}
