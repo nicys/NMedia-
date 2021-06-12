23fcc5fa62cf6a4d19a2304bcb9c7fba8d1e0d77
nicys <nicys2476@yandex.ru>
2021-06-12 13:59:22 +0700

    added @RawValue for @Parcelize of Post

--- app/src/main/java/ru/netology/dto/Post.kt
+++ app/src/main/java/ru/netology/dto/Post.kt
@@ -1,7 +1,11 @@
 package ru.netology.dto
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+import kotlinx.parcelize.RawValue
 import ru.netology.enumeration.AttachmentType
 
+@Parcelize
 data class Post(
     val id: Long,
     val author: String,
@@ -13,8 +17,8 @@ data class Post(
     val shares: String? = "0",
     val sharesCnt: Int = 0,
     val video: String? = null,
-    val attachment: Attachment? = null,
-)
+    val attachment: @RawValue Attachment? = null,
+) : Parcelable
 
 data class Attachment(
     val url: String,
