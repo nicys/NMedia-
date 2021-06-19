ed815db6e86335558bea53fcbad3c49535287280
nicys <nicys2476@yandex.ru>
2021-06-19 15:28:59 +0700

    added menu auth

--- app/src/main/java/ru/netology/AppActivity.kt
+++ app/src/main/java/ru/netology/AppActivity.kt
@@ -2,12 +2,22 @@ package ru.netology
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
+import com.google.android.gms.common.ConnectionResult
+import com.google.android.gms.common.GoogleApiAvailability
+import com.google.firebase.messaging.FirebaseMessaging
 import ru.netology.NewPostFragment.Companion.textData
+import ru.netology.auth.AppAuth
+import ru.netology.viewmodel.AuthViewModel
 
 class AppActivity : AppCompatActivity(R.layout.activity_app) {
+    private val viewModel: AuthViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +39,60 @@ class AppActivity : AppCompatActivity(R.layout.activity_app) {
                 }
             )
         }
-        lifecycleScope
+        viewModel.data.observe(this) {
+            invalidateOptionsMenu()
+        }
+
+        checkGoogleApiAvailability()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_main, menu)
+
+        menu?.let {
+            it.setGroupVisible(R.id.unauthenticated, !viewModel.authenticated)
+            it.setGroupVisible(R.id.authenticated, viewModel.authenticated)
+        }
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.signin -> {
+                // TODO: just hardcode it, implementation must be in homework
+                AppAuth.getInstance().setAuth(5, "x-token")
+                true
+            }
+            R.id.signup -> {
+                // TODO: just hardcode it, implementation must be in homework
+                AppAuth.getInstance().setAuth(5, "x-token")
+                true
+            }
+            R.id.signout -> {
+                // TODO: just hardcode it, implementation must be in homework
+                AppAuth.getInstance().removeAuth()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun checkGoogleApiAvailability() {
+        with(GoogleApiAvailability.getInstance()) {
+            val code = isGooglePlayServicesAvailable(this@AppActivity)
+            if (code == ConnectionResult.SUCCESS) {
+                return@with
+            }
+            if (isUserResolvableError(code)) {
+                getErrorDialog(this@AppActivity, code, 9000)?.show()
+                return
+            }
+            Toast.makeText(this@AppActivity, R.string.google_play_unavailable, Toast.LENGTH_LONG)
+                .show()
+        }
+
+//        FirebaseMessaging.getInstance().token.addOnSuccessListener {
+//            println(it)
+//        }
     }
 }
