4dd3f982b17307cdf071fbeaeca9fefe647f787c
nicys <nicys2476@yandex.ru>
2021-06-19 20:57:11 +0700

    renamed

--- app/src/main/java/ru/netology/AuthenFragment.kt
+++ app/src/main/java/ru/netology/AuthenFragment.kt
@@ -7,14 +7,10 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import ru.netology.databinding.FragmentAuthenBinding
-import ru.netology.dto.Post
-import ru.netology.util.PostArg
-import ru.netology.util.StringArg
+import ru.netology.databinding.AuthenFragmentBinding
 import ru.netology.viewmodel.AuthViewModel
-import ru.netology.viewmodel.PostViewModel
 
-class FragmentAuthen : Fragment() {
+class AuthenFragment : Fragment() {
 //    companion object {
 //        var Bundle.postPhoto: String? by StringArg
 //        var Bundle.postData: Post? by PostArg
@@ -30,7 +26,7 @@ class FragmentAuthen : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding = FragmentAuthenBinding.inflate(inflater, container, false)
+        val binding = AuthenFragmentBinding.inflate(inflater, container, false)
         return binding.root
     }
 }
