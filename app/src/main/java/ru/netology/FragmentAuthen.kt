cf31bba2da30692c5e5d4e52a2d91f98f40891cf
nicys <nicys2476@yandex.ru>
2021-06-19 20:51:38 +0700

    corrected code

--- app/src/main/java/ru/netology/FragmentAuthen.kt
+++ app/src/main/java/ru/netology/FragmentAuthen.kt
@@ -1,59 +1,36 @@
 package ru.netology
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import ru.netology.databinding.FragmentAuthenBinding
+import ru.netology.dto.Post
+import ru.netology.util.PostArg
+import ru.netology.util.StringArg
+import ru.netology.viewmodel.AuthViewModel
+import ru.netology.viewmodel.PostViewModel
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [FragmentAuthen.newInstance] factory method to
- * create an instance of this fragment.
- */
 class FragmentAuthen : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+//    companion object {
+//        var Bundle.postPhoto: String? by StringArg
+//        var Bundle.postData: Post? by PostArg
+//    }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    @ExperimentalCoroutinesApi
+    private val viewModel: AuthViewModel by viewModels(
+        ownerProducer = ::requireParentFragment
+    )
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_authen, container, false)
-    }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FragmentAuthen.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FragmentAuthen().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        val binding = FragmentAuthenBinding.inflate(inflater, container, false)
+        return binding.root
     }
 }
