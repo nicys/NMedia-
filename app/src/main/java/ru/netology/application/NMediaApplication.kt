1be6d7614389c23bdc7af55b10f28c192898b73c
nicys <nicys2476@yandex.ru>
2021-06-19 15:12:01 +0700

    initial NMediaApplication.kt

---
+++ app/src/main/java/ru/netology/application/NMediaApplication.kt
@@ -0,0 +1,11 @@
+package ru.netology.nmedia.application
+
+import android.app.Application
+import ru.netology.auth.AppAuth
+
+class NMediaApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        AppAuth.initApp(this)
+    }
+}
