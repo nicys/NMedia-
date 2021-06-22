879dec605f003fff1364fb0476548de3c2329844
nicys <nicys2476@yandex.ru>
2021-06-23 00:05:33 +0700

    corrected

--- app/src/main/java/ru/netology/repository/PostRepository.kt
+++ app/src/main/java/ru/netology/repository/PostRepository.kt
@@ -1,9 +1,9 @@
 package ru.netology.repository
 
 import kotlinx.coroutines.flow.Flow
+import ru.netology.dto.Media
+import ru.netology.dto.MediaUpload
 import ru.netology.dto.Post
-import ru.netology.nmedia.dto.Media
-import ru.netology.nmedia.dto.MediaUpload
 
 interface PostRepository {
 
