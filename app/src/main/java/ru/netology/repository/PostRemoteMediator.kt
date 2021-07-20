48fa29d251fba7b850f1005b384d955fdcc54777
nicys <nicys2476@yandex.ru>
2021-07-20 20:06:34 +0700

    initial PostRemoteMediator.kt

---
+++ app/src/main/java/ru/netology/repository/PostRemoteMediator.kt
@@ -0,0 +1,95 @@
+package ru.netology.repository
+
+import androidx.paging.ExperimentalPagingApi
+import androidx.paging.LoadType
+import androidx.paging.PagingState
+import androidx.paging.RemoteMediator
+import androidx.room.withTransaction
+import ru.netology.api.ApiService
+import ru.netology.dao.PostDao
+import ru.netology.dao.PostRemoteKeyDao
+import ru.netology.db.AppDb
+import ru.netology.entity.PostEntity
+import ru.netology.entity.PostRemoteKeyEntity
+import ru.netology.entity.toEntity
+import ru.netology.error.ApiError
+
+@OptIn(ExperimentalPagingApi::class)
+class PostRemoteMediator(
+    private val service: ApiService,
+    private val db: AppDb,
+    private val postDao: PostDao,
+    private val postRemoteKeyDao: PostRemoteKeyDao,
+) : RemoteMediator<Int, PostEntity>() {
+    override suspend fun load(
+        loadType: LoadType,
+        state: PagingState<Int, PostEntity>
+    ): MediatorResult {
+        try {
+            val response = when (loadType) {
+                LoadType.REFRESH -> service.getLatest(state.config.initialLoadSize)
+                LoadType.PREPEND -> {
+                    val id = postRemoteKeyDao.max() ?: return MediatorResult.Success(
+                        endOfPaginationReached = false
+                    )
+                    service.getAfter(id, state.config.pageSize)
+                }
+                LoadType.APPEND -> {
+                    val id = postRemoteKeyDao.min() ?: return MediatorResult.Success(
+                        endOfPaginationReached = false
+                    )
+                    service.getBefore(id, state.config.pageSize)
+                }
+            }
+
+            if (!response.isSuccessful) {
+                throw ApiError(response.code(), response.message())
+            }
+            val body = response.body() ?: throw ApiError(
+                response.code(),
+                response.message(),
+            )
+
+            db.withTransaction {
+                when (loadType) {
+                    LoadType.REFRESH -> {
+                        postRemoteKeyDao.removeAll()
+                        postRemoteKeyDao.insert(
+                            listOf(
+                                PostRemoteKeyEntity(
+                                    type = PostRemoteKeyEntity.KeyType.AFTER,
+                                    id = body.first().id,
+                                ),
+                                PostRemoteKeyEntity(
+                                    type = PostRemoteKeyEntity.KeyType.BEFORE,
+                                    id = body.last().id,
+                                ),
+                            )
+                        )
+                        postDao.removeAll()
+                    }
+                    LoadType.PREPEND -> {
+                        postRemoteKeyDao.insert(
+                            PostRemoteKeyEntity(
+                                type = PostRemoteKeyEntity.KeyType.AFTER,
+                                id = body.first().id,
+                            )
+                        )
+                    }
+                    LoadType.APPEND -> {
+                        postRemoteKeyDao.insert(
+                            PostRemoteKeyEntity(
+                                type = PostRemoteKeyEntity.KeyType.BEFORE,
+                                id = body.last().id,
+                            )
+                        )
+                    }
+                }
+                postDao.insert(body.toEntity())
+            }
+            return MediatorResult.Success(endOfPaginationReached = body.isEmpty())
+        } catch (e: Exception) {
+            return MediatorResult.Error(e)
+        }
+    }
+}
